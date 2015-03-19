51e124ffd196286e180567fb1b96c152be442058
Tom Fuertes <tomfuertes@gmail.com>
2015-03-19 14:39:40 -0500

    checkin base files

---
+++ exp-1-foo/exp-1-foo-control.js
@@ -0,0 +1,14 @@
+/* _optimizely_evaluate=force */ /*global $*/
+/*--------------------------------------------
+ *
+ *  test name: exp-1-foo
+ *  test platform: optimizely
+
+ *  author: tom@clearhead.me
+ *  notes:
+ *    - valid for: {{urls}}
+ *    - assumes {{tbd}}
+ *
+ * ------------------------------------------*/
+ $.exp1.log('control');
+/* _optimizely_evaluate=safe */
